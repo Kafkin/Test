c942b174dd453c8031ecdd65a3774c120bdc02fc
lleeeuuuuanchik <101695866+lleeeuuuuanchik@users.noreply.github.com>
2022-05-22 13:44:27 +0300

    123

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+const App = {
+    data() {
+        return {
+          currentPage: "home", // home, cart, reg, log, product, forget, aboutus
+        }
+    },
+}
+Vue.createApp(App).mount('#app')
